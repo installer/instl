ffcd3cc79a98efc21d5354b88af65bdd600dd151
MarvinJWendt <git@marvinjwendt.com>
2023-09-11 05:14:01 +0200

    website update

--- internal/pkg/handlers/missing-platform.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func MissingPlatform(c *fiber.Ctx) error {
-	c.Status(400)
-	return c.SendString("error: missing platform - please specify a platform in the URL (e.g. instl.sh/owner/repo/linux)")
-}
